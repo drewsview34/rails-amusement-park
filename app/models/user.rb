c9dec2abf2fef5e57f8616613c4027a68141ae18
drewsview34 <drewsview34@outlook.com>
2021-09-30 15:24:08 -0700

    added admin column to Users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    validates :password, presence: true
+    validates :admin, inclusion: false
 end
