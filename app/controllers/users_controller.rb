0a667a1eea03273ca432ed612157e968f8e96ccd
drewsview34 <drewsview34@outlook.com>
2021-09-30 21:26:07 -0700

    added sessions view pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,27 @@ class UsersController < ApplicationController
     @users = User.new
   end
 
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    if session[:user_id]
+      current_user
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :tickets, :nausea, :happiness, :height, :password, :admin)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ActiveRecord::Base
     has_many :attractions, through: :rides
 
 
-    def mood
-        if self.valid?
-            if !self.admin && self.nausea > self.happiness
-                "sad"
-            else
-                "happy"
-            end
-        end
-    end
+  def mood 
+    if self.valid?
+      if !self.admin && self.nausea > self.happiness 
+        "sad"
+      else
+        "happy"  
+      end 
+    end 
+  end
 end
